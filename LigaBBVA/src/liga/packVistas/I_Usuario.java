6fd88c6ebab8f5dd578551b6125eb0ad8d3d1eb3
spolex <jisanchez003@ikasle.ehu.es>
2014-01-19 18:58:47 +0100

    Nuevo pack para vista

---
+++ LigaBBVA/src/liga/packVistas/I_Usuario.java
@@ -0,0 +1,43 @@
+package liga.packVistas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class I_Usuario {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					I_Usuario window = new I_Usuario();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public I_Usuario() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
