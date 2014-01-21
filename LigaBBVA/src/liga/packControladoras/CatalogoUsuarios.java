5910c41160f6b4c5fc03ee66879c5959f0b8699e
jrodriguez116 <jrodriguez118@ehu.es>
2014-01-21 11:53:03 +0100

    Añadidos métodos del usuario.

--- LigaBBVA/src/liga/packControladoras/CatalogoUsuarios.java
+++ LigaBBVA/src/liga/packControladoras/CatalogoUsuarios.java
@@ -71,6 +71,42 @@ public class CatalogoUsuarios {
 		return rdo;
 	}
 	
+	public String recuperarPass(String id, String resp)
+	{
+		/*
+		 * Método que devuelve la contraseña de un usuario en función de si ha contestado bien la pregunta
+		 * de seguridad. De no ser así devolverá null.
+		 */
+		
+		String rdo=null;
+		
+		ResultadoSQL RdoSQL=SGBD.getSGBD().consultaSQL("SELECT Contrasena FROM Usuario WHERE Nombre=" + id +" AND RespuestaSeg="+resp+"");
+		if(RdoSQL.next())
+		rdo=RdoSQL.get("Contrasena");
+		
+		return rdo;
+	}
+	
+	
+	public boolean cambiarPass(String id, String passAnt, String passN, String preg, String resp)
+	{
+		/*
+		 * Método que cambia la contraseña y además actualiza pregunta y respuesta de seguridad.
+		 */
+		
+		boolean rdo=false;
+		if(identificarse(id,passAnt))
+		{
+			SGBD.getSGBD().execSQL("UPDATE Usuario SET Contrasena="+ passN+", PreguntaSeg="+preg+", RespuestaSeg="+resp+" WHERE Nombre="+id+"");
+			rdo=true;
+		}
+		/*
+		 * Como un execSQL no nos permite comprobar si se ha efectuado el cambio, comprobamos que haya introducido
+		 * la contraseña vieja correcta mediante el método identificarse(id,pass)
+		 */
+		
+		return rdo;
+	}
 	
 	
 }
