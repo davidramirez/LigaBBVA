9d6a3bcad160fb7340cc7e7ed7755bd7341292d7
spolex <jisanchez003@ikasle.ehu.es>
2014-01-22 11:42:04 +0100

    Test Case para que probéis los métodos de los catálogos

---
+++ LigaBBVA/pruebas/liga/Junits/CatalogoTemporadasTest.java
@@ -0,0 +1,61 @@
+package liga.Junits;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CatalogoTemporadasTest {
+
+	@Before
+	public void setUp() throws Exception 
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception 
+	{
+	}
+
+	@Test
+	public void testObtenerTemporadas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerJornadasDe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerJornadaAnterior() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerUltimaTemporada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerUltimaJornadaDe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerPartidosDe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInicializarTemporada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerDatosPartido() {
+		fail("Not yet implemented");
+	}
+
+}
