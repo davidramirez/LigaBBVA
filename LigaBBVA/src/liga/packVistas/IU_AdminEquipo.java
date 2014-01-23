cac2ff26c06bc71296e2e3931c93872c9673e114
byJonyB <byjonyb@GENGAR>
2014-01-23 20:45:46 +0100

    IU_AdminEquipo sólo es accesible desde IU_Inicial.

--- LigaBBVA/src/liga/packVistas/IU_AdminEquipo.java
+++ LigaBBVA/src/liga/packVistas/IU_AdminEquipo.java
@@ -1,7 +1,5 @@
 package liga.packVistas;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -17,22 +15,6 @@ import liga.packControladoras.C_Usuario;
 public class IU_AdminEquipo extends JFrame {
 
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					IU_AdminEquipo frame = new IU_AdminEquipo("AthleticUser");
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
