4a94fa4742279514bca51166b219714fe6738f61
jrodriguez116 <jrodriguez118@ehu.es>
2014-01-23 19:05:06 +0100

    Subo pruebas de mierda y correcciones en IU inicial.

--- LigaBBVA/pruebas/liga/Junits/CatalogoUsuariosTest.java
+++ LigaBBVA/pruebas/liga/Junits/CatalogoUsuariosTest.java
@@ -1,6 +1,8 @@
 package liga.Junits;
 
 import static org.junit.Assert.*;
+import liga.packControladoras.CatalogoUsuarios;
+import liga.packGestorBD.SGBD;
 
 import org.junit.After;
 import org.junit.Before;
@@ -10,35 +12,50 @@ public class CatalogoUsuariosTest {
 
 	@Before
 	public void setUp() throws Exception {
+		SGBD.getSGBD().execSQL("INSERT INTO usuario VALUES ('prueba', 'contrasena','preg','resp','1')");
+		SGBD.getSGBD().execSQL("INSERT INTO equipo VALUES ('equipoprueba', '1','1000','prueba','Vizcaya')");
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
+		SGBD.getSGBD().execSQL("DELETE FROM usuario WHERE nombre='prueba'");
 	}
 
 	@Test
 	public void testIdentificarse() {
-		fail("Not yet implemented");
+		
+		assertTrue(CatalogoUsuarios.getMiCatalogoUsuarios().identificarse("prueba", "contrasena"));
+		assertFalse(CatalogoUsuarios.getMiCatalogoUsuarios().identificarse("prueba", "contraseno"));
+		
+		
 	}
 
 	@Test
 	public void testObtenerTipo() {
-		fail("Not yet implemented");
+		String rdo = CatalogoUsuarios.getMiCatalogoUsuarios().obtenerTipo("prueba");
+		assertEquals("equipo",rdo);
 	}
 
 	@Test
 	public void testObtenerPregunta() {
-		fail("Not yet implemented");
+		String rdo = CatalogoUsuarios.getMiCatalogoUsuarios().obtenerPregunta("prueba");
+		assertEquals(rdo, "preg");
 	}
 
 	@Test
 	public void testRecuperarPass() {
-		fail("Not yet implemented");
+		String rdo = CatalogoUsuarios.getMiCatalogoUsuarios().recuperarPass("prueba", "resp");
+		assertEquals(rdo,"contrasena");
+		rdo= CatalogoUsuarios.getMiCatalogoUsuarios().recuperarPass("prueba", "rusp");
+		assertNull(rdo);
 	}
 
 	@Test
 	public void testCambiarPass() {
-		fail("Not yet implemented");
+		CatalogoUsuarios.getMiCatalogoUsuarios().cambiarPass("prueba", "contrasena", "contrasena2", "preg", "resp");
+		assertTrue(CatalogoUsuarios.getMiCatalogoUsuarios().identificarse("prueba", "contrasena2"));
 	}
 
 	@Test
@@ -49,6 +66,7 @@ public class CatalogoUsuariosTest {
 	@Test
 	public void testActualizarAdminEquipo() {
 		fail("Not yet implemented");
+	
 	}
 
 }
